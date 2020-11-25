3dd0423d7a1394b6dc85f4cdee97e7ebb09fad29
yanghelin <550488076@qq.com>
2020-11-25 20:20:52 +0800

    Netty实现Http服务器

---
+++ Netty实现http服务器/HttpServerHandler.java
@@ -0,0 +1,31 @@
+package cn.yhl.server;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.handler.codec.http.*;
+import io.netty.util.CharsetUtil;
+
+public class HttpServerHandler extends SimpleChannelInboundHandler<FullHttpRequest> {
+
+    @Override
+    protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest req) throws Exception {
+        // 获取请求的uri
+        String uri = req.uri();
+
+        String msg = "<html><head><title>DEMO</title></head><body>你请求uri为：" + uri+"</body></html>";
+        // 创建http响应
+        FullHttpResponse response = new DefaultFullHttpResponse(
+                HttpVersion.HTTP_1_1,
+                HttpResponseStatus.OK,
+                Unpooled.copiedBuffer(msg, CharsetUtil.UTF_8));
+        // 设置头信息
+        response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/html; charset=UTF-8");
+        //response.headers().set(HttpHeaderNames.CONTENT_TYPE, "text/plain; charset=UTF-8");
+        // 将html write到客户端
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE);
+    }
+
+
+}
